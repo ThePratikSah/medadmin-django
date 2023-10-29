e39819bd4d37916fe99e428e1218b2e6e6c0a29f
Pratik <pratik@etherealcovenant.com>
2023-10-30 01:28:53 +0530

    Added option for category and product upload logo

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,18 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    logo_url = models.URLField()
+    description = models.TextField(null=True, blank=True)
+    logo_url = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -28,6 +36,20 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'brands'
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
 
     def __str__(self):
         return self.name
