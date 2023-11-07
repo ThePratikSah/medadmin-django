cae1f5ad58431e7a43e7a6c258fae9be997d8bf7
Pratik <pratik@etherealcovenant.com>
2023-11-07 15:04:33 +0530

    Added path for static

--- medstore/urls.py
+++ medstore/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from medstore import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Medstore Admin'
 admin.site.site_title = 'Medstore'
