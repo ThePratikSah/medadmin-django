ea907094226dff664b8bf6d30504afb21987d72e
Pratik <pratik@etherealcovenant.com>
2023-11-07 14:44:26 +0530

    Added logic for static

--- medstore/settings.py
+++ medstore/settings.py
@@ -121,7 +121,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
