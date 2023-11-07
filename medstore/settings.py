ed218b6b03ca2481eb5d0773ce7040c584cc3f09
Pratik <pratik@etherealcovenant.com>
2023-11-07 18:36:47 +0530

    Added static path

--- medstore/settings.py
+++ medstore/settings.py
@@ -122,12 +122,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
